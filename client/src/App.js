52b2b48035300e7b01aeef8f92ec0d80caa7d33b
Jonathan Thompson <thompson.jonm@gmail.com>
2017-09-27 20:36:56 -0500

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
+// Dependencies
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+// Style
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +12,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Student Roster</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
